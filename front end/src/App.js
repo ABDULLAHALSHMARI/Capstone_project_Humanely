80fe59fefc1ed121336ad7cdfe8aa4f9013820d8
Abdullah Alshammari <92294783+ABDULLAHALSHMARI@users.noreply.github.com>
2022-01-05 16:46:33 +0300

    Add files via upload

--- front end/src/App.js
+++ front end/src/App.js
@@ -1,29 +1,37 @@
 import React from "react";
 import {Outlet, Link } from "react-router-dom";
 import './App.css'
+import { useState } from "react";
+import Login from "./combonents/Login";
+import Home from "./combonents/home";
+
 export default function App() {
+
+  let [authenticate, setAuthenticate] = useState(false);
+
+  function handleLogin() {
+    setAuthenticate(true);
+  }
+  if (!authenticate) {
+    return <div class="login"><Login handleLogin={handleLogin} /> </div>;
+  }
+
   return (
     <div style={{backgroundimage:"kabsa.jpg"}}>
       {/* <h1 style={{ backgroundColor: ""
       }}>humanly</h1> */}
-      <nav
-        // style={{
-        //   borderBottom: "solid 1px",
-        //   paddingBottom: "1rem",
-        //   backgroundColor: "lightblue"
-        // }}
-      >
+      <nav>
         <div class="header">
   <a href="#default" class="logo">Humanly</a>
   <div class="header-right">
-
+      
     <Link className="active" to="/User">Register</Link>  
  
     <Link to="/Services">Services</Link>  
 
     <Link to="/Home">Home</Link>  
     <Link to="/Login">Login</Link>
-
+    
   </div>
 </div>
  
