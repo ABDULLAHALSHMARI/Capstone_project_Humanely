a8a70267f0519f7492f394181a5025d7363405da
Abdullah Alshammari <92294783+ABDULLAHALSHMARI@users.noreply.github.com>
2022-01-18 23:46:40 +0300

    Add files via upload

--- front end/src/App.js
+++ front end/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Outlet, Link } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 import './App.css'
 import { useState } from "react";
 import Login from "./combonents/Login";
@@ -7,105 +7,87 @@ import Home from "./combonents/home";
 import Food from "./combonents/Food";
 
 export default function App() {
+  let [logged, setLogged] = useState("");
 
-  // function Detel(){
-  //   if(localStorage.setItem("logined","yes")){
-  //       <Link to="/"><button onClick={handleSubmit} className='btn'>Login</button></Link>
-  //   }
-  //   else{
-  //       <Link to="/"><button onClick={handleSubmit} className='btn'>Logout</button></Link>
-  //   }
-
-
-
-
-//   let [authenticate, setAuthenticate] = useState(false);
-
-//  let usery= sessionStorage.setItem("enter", {handleLogin});
-  
-//   function handleLogin() {
-//     setAuthenticate(true);
-//   }
-//   if (!authenticate) {
-//     return <div class="login"><Login handleLogin={handleLogin} /> </div>;
-//   }
-
-//   function (event){
-//     setAuthenticate(true)
-//     if  (!authenticate){
-    
-//         return(<Food/>);
-//     }
-//     else{}
-// }
-  return (
-    <div style={{backgroundimage:""}}>
-      {/* <h1 style={{ backgroundColor: ""
-      }}>humanly</h1> */}
-      
-      {/* <nav>
-        <div class="header">
-  <a href="<home/>" class="logo">Humanly</a>
-  <div class="header-right">
-      
-    <Link className="active" to="/User">Register</Link>  
  
-    <Link to="/Services">Services</Link>  
+if(localStorage.getItem("logined") == "yes"){
+  return (
+    <div style={{ backgroundimage: "" }}>
 
-    <Link to="/Home">Home</Link>  
-    <Link to="/Login">Login</Link>
-
-    <button onClick={()=>{localStorage.setItem("logined","no")}}>Logout</button>
-    <hr/>
-    <br/>
-    
-
-   
-  </div>
-</div>
-
-      </nav>
-      <Outlet/>
-      <div className="body"> */}
-      
-    {/* <img src="https://www.alwatan.com.sa/uploads/imported_images/31/16/73/dbsa.jpg"/>
-    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWuVn7LD2lno7ABfLINRoNWQU7v5zBRIIh0A&usqp=CAU"/> */}
-      {/* </div> */}
       <header>
-     <div>
-         <h4>Winter Sale - Save 50%</h4>
-     </div>
-     <div>
-         <h2>only one</h2>
-     </div>
-     <div>
-         <ul>
-            <li><Link className="active" to="/User">Register</Link>  
- </li>
-             <div class="dropdown">
-                    <li class="dropbtn"><Link to="/Services">Services</Link>  </li>
-                    <div class="dropdown-content">
-     
-      </div>
-            </div>
-             <div class="dropdown">
-                    <li class="dropbtn"><Link to="/Home">Home</Link>  </li>
-                  
-            </div>
-            
+        <div>
+          
+          <h4>Hand in hand let's continue</h4>
+        </div>
+        <div>
+          <img  className="logo" src="https://files.slack.com/files-pri/T02FM3FE1DJ-F02UFTKG76F/logo.png"/>
+          <h2>Takatof Project</h2>
+        </div>
+        <div>
+          <ul>
+            <li><Link className="active" to="/User">Register</Link>
+            </li>
             <div class="dropdown">
-                    <li class="dropbtn"><Link to="/Login">Login</Link>
-</li>
-<div class="dropdown">
+              <li class="dropbtn"><Link to="/Services">Services</Link>  </li>
+              <div class="dropdown-content">
 
-<li class="dropbtn"><p onClick={()=>{localStorage.setItem("logined","no")}}>Logout</p>
-</li></div>
-                   
+              </div>
             </div>
-         </ul>
-     </div>
- </header>
- <Outlet/>
+            <div class="dropdown">
+              <li class="dropbtn"><Link to="/Home">Home</Link>  </li>
+
+            </div>
+            <div class="dropdown">
+
+              <li class="dropbtn"><Link to="" onClick={() => { localStorage.setItem("logined", "no"); setLogged((logged = "no")) }}>Logout</Link>
+              </li>
+            </div>
+
+            
+            
+
+
+          </ul>
+        </div>
+      </header>
+      <Outlet />
     </div>
   );
-  }
+} else{
+  return (
+    <div style={{ backgroundimage: "" }}>
+
+      <header>
+        <div>
+          <h4>Hand in hand let's continue</h4>
+        </div>
+        <div>
+          <h2>Takatof Project</h2>
+        </div>
+        <div>
+          <ul>
+            <li><Link className="active" to="/User">Register</Link>
+            </li>
+            <div class="dropdown">
+              <li class="dropbtn"><Link to="/Services">Services</Link>  </li>
+              <div class="dropdown-content">
+
+              </div>
+            </div>
+            <div class="dropdown">
+              <li class="dropbtn"><Link to="/Home">Home</Link>  </li>
+
+            </div>
+            <div class="dropdown">
+              <li class="dropbtn"><Link to="/Login">Login</Link>
+              </li>
+            </div>
+
+
+          </ul>
+        </div>
+      </header>
+      <Outlet />
+    </div>
+  );
+}}
